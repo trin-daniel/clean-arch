5f5a012bb94b4dba0c2211263b7d45ede0927065
trindaniel <trindadedanniel63@outlook.com>
2020-07-18 21:33:18 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,4 +13,18 @@ describe('component signUp controller', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return error 400 if not specify e-mail of client', () => {
+    const systemUnderTest = new SignUpController()
+    const request = {
+      body: {
+        name: 'any_email@gmail.com',
+        password: 'any_password',
+        confirmation: 'value equal of password field'
+      }
+    }
+    const response = systemUnderTest.handle(request)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new Error('Missing param: email'))
+  })
 })
