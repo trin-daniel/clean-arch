577d33a3cdaa8dea3edf1e442d3f68c9df2f2efc
trindaniel <trindadedanniel63@outlook.com>
2020-07-19 00:30:01 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { badRequest } from '../helpers/http-helper'
 import { Request, Response } from '../protocols/http'
 export class SignUpController {
   handle (request: Request): Response {
-    if (!request.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!request.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!request.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
