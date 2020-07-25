1a5b2c6a16212cbad02803a4d544297fe5d1d02e
trindaniel <trindadedanniel63@outlook.com>
2020-07-25 12:38:39 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  public async add (account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(account)
+    const contentAccount = result.ops[0]
+    const { _id: id, ...rest } = contentAccount
+    return { id, ...rest }
+  }
+}
