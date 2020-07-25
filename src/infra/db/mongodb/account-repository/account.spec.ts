e5fcebd77c59fa5070a692ee24933395d2f59d8b
trindaniel <trindadedanniel63@outlook.com>
2020-07-25 12:38:59 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+declare const process:{
+  env: {
+    MONGO_URL: string
+  }
+}
+describe('Account mongo repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const systemUnderTest = new AccountMongoRepository()
+    const account = await systemUnderTest.add({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@gmail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
