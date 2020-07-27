09de37bc219b10b53562f46d7ac922cd7dd8b8f9
trindaniel <trindadedanniel63@outlook.com>
2020-07-27 14:55:59 -0300

    refactor: move system under test creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -6,25 +6,41 @@ import
   HttpResponse
 } from '../../presentation/protocols'
 
+interface SystemUnderTest {
+  systemUnderTest:LogControllerDecorator
+  controllerStub: Controller,
+}
+const makeController = ():Controller => {
+  class ControllerStub implements Controller {
+    public async handle (request:HttpRequest):Promise<HttpResponse> {
+      const httpResponse:HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'any_name',
+          email: 'any_email@gmail.com',
+          password: 'any_password',
+          confirmation: 'any_password'
+        }
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSystemUnderTest = ():SystemUnderTest => {
+  const controllerStub = makeController()
+  const systemUnderTest = new LogControllerDecorator(controllerStub)
+  return {
+    systemUnderTest,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      public async handle (request:HttpRequest):Promise<HttpResponse> {
-        const httpResponse:HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'any_name',
-            email: 'any_email@gmail.com',
-            password: 'any_password',
-            confirmation: 'any_password'
-          }
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { controllerStub, systemUnderTest } = makeSystemUnderTest()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const systemUnderTest = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'any_name',
