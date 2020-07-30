fcab098535e8595f1493235900811d8bd9bb251c
trindaniel <trindadedanniel63@outlook.com>
2020-07-30 10:49:25 -0300

    refactor: move system under test creation to a factory method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,18 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '../../errors'
 
+const makeSystemUnderTest = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
 describe('RequiredFieldValidation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const systemUnderTest = new RequiredFieldValidation('field')
+    const systemUnderTest = makeSystemUnderTest()
     const error = systemUnderTest.validate({ any_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should not return if validation succeeds', () => {
-    const systemUnderTest = new RequiredFieldValidation('field')
+    const systemUnderTest = makeSystemUnderTest()
     const error = systemUnderTest.validate({ field: 'any_value' })
     expect(error).toBeFalsy()
   })
