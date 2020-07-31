b17cb7275c1e075be3eba992528e8b91140b1c9f
trindaniel <trindadedanniel63@outlook.com>
2020-07-31 16:01:44 -0300

    refactor: move duplicated code to helper methods

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,27 +1,51 @@
 import { AccountModel } from '../../../domain/models/account'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
+
+const makeFakeAccount = ():AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@gmail.com',
+  password: 'any_password'
+})
+
+const makeLoadAccountByEmailRepository = ():LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    public async load (email:string):Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+const makeFakeAuthentication = ():AuthenticationModel => ({
+  email: 'any_email@gmail.com',
+  password: 'any_password'
+})
+
+interface SystemUnderTestTypes{
+  systemUnderTest:DbAuthentication,
+  loadAccountByEmailRepositoryStub:LoadAccountByEmailRepository
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
+  const systemUnderTest = new DbAuthentication(loadAccountByEmailRepositoryStub)
+  return {
+    loadAccountByEmailRepositoryStub,
+    systemUnderTest
+  }
+}
 
 describe('DbAuthentication usecase', () => {
   test('Should call LoadAccountByEmailRepository with correct  email', async () => {
-    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-      public async load (email:string):Promise<AccountModel> {
-        const account:AccountModel = {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email@gmail.com',
-          password: 'any_password'
-        }
-        return new Promise(resolve => resolve(account))
-      }
-    }
-    const loadByAccountRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const systemUnderTest = new DbAuthentication(loadByAccountRepositoryStub)
-    const loadSpy = jest.spyOn(loadByAccountRepositoryStub, 'load')
-    await systemUnderTest.auth({
-      email: 'any_email@gmail.com',
-      password: 'any_password'
-    })
+    const {
+      systemUnderTest,
+      loadAccountByEmailRepositoryStub
+    } = makeSystemUnderTest()
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    await systemUnderTest.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_email@gmail.com')
   })
 })
