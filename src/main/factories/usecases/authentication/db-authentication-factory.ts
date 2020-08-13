a17cad73ad4ee32635f1cf1a8752850462973965
trindaniel <trindadedanniel63@outlook.com>
2020-08-12 21:01:11 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,15 @@
+import { env } from '../../../config/env'
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import { Authentication } from '../../../../domain/usecases/authentication'
+
+export const makeDbAuthentication = ():Authentication => {
+  return new DbAuthentication(
+    new AccountMongoRepository(),
+    new BcryptAdapter(12),
+    new JwtAdapter(env.secret),
+    new AccountMongoRepository()
+  )
+}
