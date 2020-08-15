208ebe8143f9369ff08f5ef8295da304537842b3
trindaniel <trindadedanniel63@outlook.com>
2020-08-15 19:32:33 -0300

    refactor: separate tests in describe blocks

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -15,55 +15,58 @@ const makeSystemUnderTest = ():BcryptAdapter => {
 }
 
 describe('Cryptographic password layer', () => {
-  test('Should call hash with correct value', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
+  describe('BcryptAdpter method hash', () => {
+    test('Should call hash with correct value', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
 
-    await systemUnderTest.hash('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
-  })
+      await systemUnderTest.hash('any_value')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
 
-  test('Should return a valid hash on hash success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const hash = await systemUnderTest.hash('any_value')
-    expect(hash).toBe('hash_final')
-  })
+    test('Should return a valid hash on hash success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const hash = await systemUnderTest.hash('any_value')
+      expect(hash).toBe('hash_final')
+    })
 
-  test('Should throw if hash throws', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    jest.spyOn(bcrypt, 'hash')
-      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise = systemUnderTest.hash('any_value')
-    await expect(promise).rejects.toThrow()
+    test('Should throw if hash throws', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      jest.spyOn(bcrypt, 'hash')
+        .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+      const promise = systemUnderTest.hash('any_value')
+      await expect(promise).rejects.toThrow()
+    })
   })
+  describe('BcryptAdpter method compare', () => {
+    test('Should call compare with correct value', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
 
-  test('Should call compare with correct value', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await systemUnderTest.compare('any_value', 'any_hash')
+      expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    })
+    test('Should return true when compare succeeds', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const available = await systemUnderTest.compare('any_value', 'any_hash')
+      expect(available).toBe(true)
+    })
+    test('Should return false when compare fails', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      jest.spyOn(bcrypt, 'compare')
+        .mockReturnValueOnce(
+          new Promise(resolve => resolve(false))
+        )
+      const available = await systemUnderTest.compare('any_value', 'any_hash')
+      expect(available).toBe(false)
+    })
 
-    await systemUnderTest.compare('any_value', 'any_hash')
-    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
-  })
-  test('Should return true when compare succeeds', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const available = await systemUnderTest.compare('any_value', 'any_hash')
-    expect(available).toBe(true)
-  })
-  test('Should return false when compare fails', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    jest.spyOn(bcrypt, 'compare')
-      .mockReturnValueOnce(
-        new Promise(resolve => resolve(false))
-      )
-    const available = await systemUnderTest.compare('any_value', 'any_hash')
-    expect(available).toBe(false)
-  })
-
-  test('Should throw if compare throws', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    jest.spyOn(bcrypt, 'compare')
-      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise = systemUnderTest.compare('any_value', 'any_hash')
-    await expect(promise).rejects.toThrow()
+    test('Should throw if compare throws', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      jest.spyOn(bcrypt, 'compare')
+        .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+      const promise = systemUnderTest.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -19,54 +19,61 @@ describe('Account mongo repository', () => {
   const makeSystemUnderTest = ():AccountMongoRepository => {
     return new AccountMongoRepository()
   }
-  test('Should return an account on add success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const account = await systemUnderTest.add({
-      name: 'any_name',
-      email: 'any_email@gmail.com',
-      password: 'any_password'
+
+  describe('AccountMongoRepository method add', () => {
+    test('Should return an account on add success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const account = await systemUnderTest.add({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@gmail.com')
+      expect(account.password).toBe('any_password')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@gmail.com')
-    expect(account.password).toBe('any_password')
   })
 
-  test('Should return an account on loadByEmail success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    await accountCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@gmail.com',
-      password: 'any_password'
+  describe('AccountMongoRepository method loadByEmail', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      })
+      const account = await systemUnderTest.loadByEmail('any_email@gmail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@gmail.com')
+      expect(account.password).toBe('any_password')
     })
-    const account = await systemUnderTest.loadByEmail('any_email@gmail.com')
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@gmail.com')
-    expect(account.password).toBe('any_password')
+
+    test('Should return null if loadByEmail fails', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const account = await systemUnderTest.loadByEmail('any_email@gmail.com')
+      expect(account).toBeFalsy()
+    })
   })
 
-  test('Should return null if loadByEmail fails', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const account = await systemUnderTest.loadByEmail('any_email@gmail.com')
-    expect(account).toBeFalsy()
-  })
+  describe('AccountMongoRepository method updateAccessToken', () => {
+    test('Should update the account accessToken on updateAccessToken success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      const result = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      })
+      const fakeAccount = result.ops[0]
+      expect(fakeAccount.accessToken).toBeFalsy()
 
-  test('Should update the account accessToken on updateAccessToken success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    const result = await accountCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@gmail.com',
-      password: 'any_password'
+      await systemUnderTest.updateAccessToken(fakeAccount._id, 'any_token')
+      const account = await accountCollection.findOne({ _id: fakeAccount._id })
+      expect(account).toBeTruthy()
+      expect(account.accessToken).toBe('any_token')
     })
-    const fakeAccount = result.ops[0]
-    expect(fakeAccount.accessToken).toBeFalsy()
-
-    await systemUnderTest.updateAccessToken(fakeAccount._id, 'any_token')
-    const account = await accountCollection.findOne({ _id: fakeAccount._id })
-    expect(account).toBeTruthy()
-    expect(account.accessToken).toBe('any_token')
   })
 })
