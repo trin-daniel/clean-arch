9dd9bc1b4f295583d81308bf71ecb7b0611e9a36
trindaniel <trindadedanniel63@outlook.com>
2020-08-15 12:42:39 -0300

    test: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,9 +1,12 @@
 import { Decrypter } from '../../protocols/cryptography/decrypter'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
 interface SystemUnderTestTypes{
   systemUnderTest: DbLoadAccountByToken,
   decrypterStub: Decrypter
+  loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository
 }
 
 const makeDecrypter = (): Decrypter => {
@@ -15,18 +18,39 @@ const makeDecrypter = (): Decrypter => {
   return new DecrypterStub()
 }
 
+const makeFakeAccount = ():AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@gmail.com',
+  password: 'hashed_password'
+})
+
+const makeLoadAccountByTokenRepository = (): LoadAccountByTokenRepository => {
+  class LoadAccountByTokenRepositoryStub implements LoadAccountByTokenRepository {
+    public async loadByToken (token:string, role?:string):Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByTokenRepositoryStub()
+}
+
 const makeSystemUnderTest = (): SystemUnderTestTypes => {
   const decrypterStub = makeDecrypter()
-  const systemUnderTest = new DbLoadAccountByToken(decrypterStub)
+  const loadAccountByTokenRepositoryStub = makeLoadAccountByTokenRepository()
+  const systemUnderTest = new DbLoadAccountByToken(decrypterStub, loadAccountByTokenRepositoryStub)
   return {
     systemUnderTest,
-    decrypterStub
+    decrypterStub,
+    loadAccountByTokenRepositoryStub
   }
 }
 
 describe('DbLoadAccountByToken', () => {
   test('Should call Descrypter with correct values', async () => {
-    const { systemUnderTest, decrypterStub } = makeSystemUnderTest()
+    const {
+      systemUnderTest,
+      decrypterStub
+    } = makeSystemUnderTest()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
 
     await systemUnderTest.load('any_token', 'any_role')
@@ -42,5 +66,16 @@ describe('DbLoadAccountByToken', () => {
 
     const account = await systemUnderTest.load('any_token', 'any_role')
     expect(account).toBeNull()
+  })
+
+  test('Should call LoadAccountByTokenRepository with correct values', async () => {
+    const {
+      systemUnderTest,
+      loadAccountByTokenRepositoryStub
+    } = makeSystemUnderTest()
+    const loadByTokenSpy = jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
+
+    await systemUnderTest.load('any_token', 'any_role')
+    expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
 })
