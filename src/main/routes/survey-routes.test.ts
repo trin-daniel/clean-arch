3a4eb1e8ebeda3ab93586f19bed834cba093bbb3
trindaniel <trindadedanniel63@outlook.com>
2020-08-16 18:04:35 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import app from '../config/app'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import { env } from '../config/env'
 
-let surveyCollection: Collection
+let surveys: Collection
+let accounts:Collection
 
 describe('AddSurvey Routes', () => {
   beforeAll(async () => {
@@ -13,20 +16,53 @@ describe('AddSurvey Routes', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection('surveys')
-    await surveyCollection.deleteMany({})
+    surveys = await MongoHelper.getCollection('surveys')
+    accounts = await MongoHelper.getCollection('accounts')
+    await surveys.deleteMany({})
+    await accounts.deleteMany({})
   })
 
-  test('Should return 403 on add survey withdout accessToken', async () => {
-    await request(app).post('/api/surveys').send({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }]
+  describe('POST/surveys', () => {
+    test('Should return 403 on add survey withdout accessToken', async () => {
+      await request(app).post('/api/surveys').send({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }]
+      })
+        .expect(403)
     })
-      .expect(403)
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const account = await accounts.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@gmail.com',
+        password: 'valid_password',
+        role: 'admin'
+      })
+      const id = account.ops[0]._id
+      const accessToken = sign({ id }, env.secret)
+      await accounts.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            answer: 'any_answer',
+            image: 'https://images.com.br/profile?image=1'
+          }]
+        })
+        .expect(204)
+    })
   })
 })
