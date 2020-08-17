1a7688d91223535c7ed772e43facce87f05c8edd
trindaniel <trindadedanniel63@outlook.com>
2020-08-17 15:10:40 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -2,8 +2,12 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { Collection } from 'mongodb'
 
+const makeSystemUnderTest = ():SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
 let surveyCollection: Collection
-describe('Survey mongo repository', () => {
+describe('SurveyMongoRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -11,27 +15,27 @@ describe('Survey mongo repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
   })
 
-  const makeSystemUnderTest = ():SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
-  test('Should add a survey on success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    await systemUnderTest.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      date: new Date()
+  describe('SurveyMongoRepository method add', () => {
+    test('Should add a survey on success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      await systemUnderTest.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
