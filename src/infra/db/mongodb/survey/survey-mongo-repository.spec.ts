2396ed23c54981a4332e37b89f65946fb160e4c9
trindaniel <trindadedanniel63@outlook.com>
2020-08-17 15:35:31 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,9 +1,31 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { Collection } from 'mongodb'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 
 const makeSystemUnderTest = ():SurveyMongoRepository => {
   return new SurveyMongoRepository()
+}
+
+const makeFakeSurveys = (): AddSurveyModel[] => {
+  return [
+    {
+      question: 'any_question',
+      answers: [{
+        answer: 'any_answer',
+        image: 'any_image'
+      }],
+      date: new Date()
+    },
+    {
+      question: 'other_question',
+      answers: [{
+        answer: 'other_answer',
+        image: 'other_image'
+      }],
+      date: new Date()
+    }
+  ]
 }
 
 let surveyCollection: Collection
@@ -36,6 +58,18 @@ describe('SurveyMongoRepository', () => {
       })
       const survey = await surveyCollection.findOne({ question: 'any_question' })
       expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('SurveyMongoRepository method loadAll', () => {
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany(makeFakeSurveys())
+      const systemUnderTest = makeSystemUnderTest()
+      const surveys = await systemUnderTest.loadAll()
+
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
   })
 })
