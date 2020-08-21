9e6dae47bf4c02db5389aa76ffd01f491dd08e82
trindaniel <trindadedanniel63@outlook.com>
2020-08-21 01:10:01 -0300

    test: ensure DbloadSurveyById calls LoadSurveyByIdRepository with correct id

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,55 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveyByIdRepository } from '../../protocols/db/survey/load-survey-by-id-repository'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+import { set, reset } from 'mockdate'
+
+type SystemUnderTestTypes = {
+  systemUnderTest: DbLoadSurveyById,
+  loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    answer: 'any_answer',
+    image: 'any_image'
+  }],
+  date: new Date()
+})
+
+const makeLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById (id: string):Promise<SurveyModel> {
+      return new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+const makeSystemUnderTest = ():SystemUnderTestTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepository()
+  const systemUnderTest = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+  return {
+    systemUnderTest,
+    loadSurveyByIdRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyById', () => {
+  beforeAll(() => {
+    set(new Date())
+  })
+
+  afterAll(() => {
+    reset()
+  })
+
+  test('Should call LoadSurveyByIdRepository', async () => {
+    const { systemUnderTest, loadSurveyByIdRepositoryStub } = makeSystemUnderTest()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+    await systemUnderTest.loadById('any_id')
+
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
