0a47952f04036026791998cd70c8ee00c612aa42
trindaniel <trindadedanniel63@outlook.com>
2020-08-20 21:06:18 -0300

    fix: make controllers return the error message instead of the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,7 @@ export const setupRoutes = (app: Express):void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
