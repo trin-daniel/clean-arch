74985f1e94985ac87402ec5d8a85df96aea72b13
trindaniel <trindadedanniel63@outlook.com>
2020-08-22 14:03:08 -0300

    test: ensure SaveSurveyResultController calls LoadSurveyById with correct values

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -0,0 +1,57 @@
+import {
+  HttpRequest,
+  LoadSurveyById,
+  SurveyModel
+} from './save-survey-result-controller-protocols'
+
+import { SaveSurveyResultController } from './save-survey-result-controller'
+
+type SystemUnderTestTypes = {
+  loadSurveyByIdStub: LoadSurveyById
+  systemUnderTest: SaveSurveyResultController
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_survey_id'
+  }
+})
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    public async loadById (id: string):Promise<SurveyModel> {
+      return new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+  return new LoadSurveyByIdStub()
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const loadSurveyByIdStub = makeLoadSurveyById()
+  const systemUnderTest = new SaveSurveyResultController(loadSurveyByIdStub)
+
+  return {
+    systemUnderTest,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  test('Should call LoadSurveyById with correct values', async () => {
+    const { systemUnderTest, loadSurveyByIdStub } = makeSystemUnderTest()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+
+    await systemUnderTest.handle(makeFakeRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
