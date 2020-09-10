5a14b4866b097ce5e510008555b2193069075bf9
trin-daniel <trindadedanniel63@outlook.com>
2020-09-10 16:37:25 -0300

    fix: bug in mongodb-memory-server

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
-    instance: {
-      dbName: 'test'
-    },
     binary: {
-      version: 'latest',
+      version: '4.0.3',
       skipMD5: true
+    },
+    instance: {
+      dbName: 'jest'
     },
     autoStart: false
   }
