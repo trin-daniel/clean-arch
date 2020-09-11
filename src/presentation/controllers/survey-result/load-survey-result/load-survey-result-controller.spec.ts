5b05a5688456da07178f89abc34920554508b069
trin-daniel <trindadedanniel63@outlook.com>
2020-09-11 18:28:32 -0300

    test: ensure LoadSurveyResultController returns 403 if an invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,22 +1,31 @@
 import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import { SurveyModel } from '../../../../domain/models/survey'
-
+import { forbidden } from '../../../helpers/http/http-helper'
+import { InvalidParamErrors } from '../../../errors/'
 type SutTypes = {
 sut: LoadSurveyResultController,
 loadSurveyByIdStub: LoadSurveyById
 }
 
+const mockSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
 const mockRequest = (): HttpRequest => ({
-  params: {
-    surveyId: 'any_id'
-  }
+  params: { surveyId: 'any_id' }
 })
 
 const mockLoadSurveyById = (): LoadSurveyById => {
   class LoadSurveyByIdStub implements LoadSurveyById {
     public async loadById (id: string): Promise<SurveyModel> {
-      return Promise.resolve(null)
+      return Promise.resolve(mockSurvey())
     }
   }
   return new LoadSurveyByIdStub()
@@ -38,5 +47,13 @@ describe('LoadSurveyResult Controller', () => {
     await sut.handle(mockRequest())
 
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const response = await sut.handle(mockRequest())
+
+    expect(response).toEqual(forbidden(new InvalidParamErrors('surveyId')))
   })
 })
